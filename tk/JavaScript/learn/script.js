77ce4b95de467b9f4e2565707db455dd38ea9037
Fikkung <125670574+Fikkung@users.noreply.github.com>
2023-06-16 00:00:08 +0700

    first commit

---
+++ tk/JavaScript/learn/script.js
@@ -0,0 +1,86 @@
+// **console.log('Hello,wrold');
+// document.getElementById("text").innerHTML ='thakdanai khamthong';
+// document.write("hello documant");
+// window.alert("what are you doing from Box");/ */
+
+
+// var price1 = 5
+// var price2 = 6
+// var total = price1+price2;
+// console.log(total);
+
+
+// var fistname = "thakdanai";
+// var lastname = "khamthong";
+// var fullname = fistname+" "+lastname;
+// console.log(fullname)
+
+
+// function functionMe(value1, value2){
+//     return value1*value2;
+// }
+// console.log(functionMe(6,6));
+
+
+
+// function changbg(){
+//     document.body.style.backgroundColor = "blue";
+// }
+ // changbg();
+
+
+
+//  function ctf(celsius){
+//     var cTem = celsius
+//     var ctofahr = cTem *9/5+32;
+//     var massage = cTem+"\xB0C"+" is "+ctofahr+" \xB0F";
+//  console.log(massage);
+// } 
+// ctf(30)
+// function Ftc(fahrenneit){
+//     var fTem = fahrenneit
+//     var ftoc = (fTem - 32) *5 /9;
+//     var massage = fTem+" \xB0F"+" is "+ftoc+" \xB0c"
+//     console.log(massage)
+// }   
+// Ftc(86)
+
+
+
+
+// var myCat={
+//     brand: "ford",
+//     modle: "1",
+//     color: "black",
+// }
+// console.log(myCat.brand);
+// console.log(myCat["brand"]);
+
+// var Person = {
+//     firstName : "thakdanai",
+//     lastName : "khamthong",
+//     fullname: function() {
+//         return this.firstName + " " + this.lastName;
+//     }
+// };
+// console.log(Person.fullname());
+
+
+
+
+// function displyDate() {
+//     document.getElementById('demo').innerHTML = Date();
+// }
+
+
+
+
+// var y = "asdasda\"asdads\"asdasd;asdasd;asdads;s \nasdasd"
+// var x = "thakdanai \"fix\" khamthong";
+// console.log(x);
+// console.log(y);
+
+
+
+
+
